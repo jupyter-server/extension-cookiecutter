726b920b0273a4290ddfe73301d92fbbf70c52ba
Zach Sailer <zsailer@apple.com>
2022-05-05 09:36:43 -0700

    use extension app

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,31 +1,10 @@
 """{{cookiecutter.project_short_description}}"""
-from pathlib import Path
-
-from .handlers import setup_handlers
+from .extension import Extension
 __version__ = "0.1.0"
-
-
-
-HERE = Path(__file__).parent.resolve()
 
 
 def _jupyter_server_extension_points():
     return [{
-        "module": "{{ cookiecutter.package_name }}"
+        "module": "{{ cookiecutter.package_name | replace('-', '_')}}",
+        "app": Extension
     }]
-
-
-def _load_jupyter_server_extension(server_app):
-    """Registers the API handler to receive HTTP requests from the frontend extension.
-
-    Parameters
-    ----------
-    server_app: ServerApp or ExtensionApp
-        Jupyter Server application instance
-    """
-    setup_handlers(server_app.web_app)
-    server_app.log.info("Registered {name} server extension".format(**data))
-
-
-# For backward compatibility with notebook server - useful for Binder/JupyterHub
-load_jupyter_server_extension = _load_jupyter_server_extension
--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/handlers.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/handlers.py
@@ -1,24 +1,16 @@
 import json
 
+from jupyter_server.extension.handler import ExtensionHandlerMixin
 from jupyter_server.base.handlers import APIHandler
-from jupyter_server.utils import url_path_join
 import tornado
 
-class RouteHandler(APIHandler):
+
+class RouteHandler(ExtensionHandlerMixin, APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
     def get(self):
         self.finish(json.dumps({
-            "data": "This is /{{ cookiecutter.package_name | replace('_', '-') }}/get_example endpoint!"
+            "data": "This is /{{ cookiecutter.package_name | replace('-', '_') }}/get_example endpoint!"
         }))
-
-
-def setup_handlers(web_app):
-    host_pattern = ".*$"
-
-    base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "{{ cookiecutter.package_name | replace('_', '-') }}", "get_example")
-    handlers = [(route_pattern, RouteHandler)]
-    web_app.add_handlers(host_pattern, handlers)
