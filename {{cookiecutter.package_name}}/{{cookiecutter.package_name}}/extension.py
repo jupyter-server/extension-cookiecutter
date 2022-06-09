fa7121a146fd7f92431e1f7845ee42fd89373c55
Zach Sailer <zsailer@apple.com>
2022-06-09 10:09:41 -0700

    add example of configurable trait

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
@@ -1,12 +1,21 @@
+from traitlets import Unicode
+
 from jupyter_server.extension.application import ExtensionApp
-from .handlers import RouteHandler
+from .handlers import PingHandler
 
 
 class Extension(ExtensionApp):
 
     name = "{{ cookiecutter.package_name | replace('-', '_') }}"
     handlers = [
-        ("{{ cookiecutter.package_name | replace('-', '_') }}/get_example", RouteHandler)
+        ("ping", PingHandler)
     ]
 
-    # mytrait = Unicode("").tag(config=True)
+    # Example of a configurable trait. This is meant to be replaced
+    # with configurable traits for this extension.
+    ping_response = Unicode(default_value="pong").tag(config=True)
+
+    def initialize_settings(self):
+        self.settings.update({
+            "ping_response": self.ping_response
+        })
