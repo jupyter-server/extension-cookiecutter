5e18cb9072aad0cb9fca9e4faba433aaea5223e9
Sam Bloomquist <bloom246@umn.edu>
2022-09-15 09:26:38 -0500

    add extension name to endpoint route (#16)

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/tests/test_handlers.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/tests/test_handlers.py
@@ -2,7 +2,7 @@ import json
 
 
 async def test_get(jp_fetch):
-    response = await jp_fetch("ping")
+    response = await jp_fetch("{{ cookiecutter.package_name | replace('_', '-') }}/ping")
 
     assert response.code == 200
     payload = json.loads(response.body)
